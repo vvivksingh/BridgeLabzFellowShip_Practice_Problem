d797777d20c210e981c89fa6e2bf467479aaec31
Vivek <vvivksingh@gmail.com>
2021-12-30 18:13:32 +0530

    welcome massege added in Address Book

---
+++ Day 9 Practice Problem/src/AddressBookMain.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AddressBookMain {
+    static String greeting = "**Welcome to Address Book Program**";
+
+    public static void main(String[] args) {
+        System.out.println(greeting);
+
+    }
+}
