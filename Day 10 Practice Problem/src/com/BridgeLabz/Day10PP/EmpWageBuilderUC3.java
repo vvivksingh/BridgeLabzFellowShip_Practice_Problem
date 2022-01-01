500ce523022a38fefffb88f41bd5c903d7d77837
Vivek <vvivksingh@gmail.com>
2022-01-01 16:12:15 +0530

    Employe wage use case 3 added

---
+++ Day 10 Practice Problem/src/com/BridgeLabz/Day10PP/EmpWageBuilderUC3.java
@@ -0,0 +1,27 @@
+package com.BridgeLabz.Day10PP;
+
+public class EmpWageBuilderUC3 {
+    public static void main(String[] args) {
+        int IS_FULL_TIME =1;
+        int IS_PART_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHours = 0;
+        int empWage = 0;
+        //Checking if present or not
+        double empCheck = Math.floor(Math.random()*10) % 3;
+        if (empCheck == IS_FULL_TIME){
+            empHours = 8;
+            System.out.println("Employee is Present");
+        }
+        else if(empCheck == IS_PART_TIME) {
+            empHours = 4;
+        }
+        else{
+            empHours = 0;
+            System.out.println("Employee is Absent");
+        }
+        empWage = empHours*EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage : "+ empWage);
+
+    }
+}
