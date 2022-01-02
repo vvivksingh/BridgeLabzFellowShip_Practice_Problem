e07206b356903d87d665d867c4be21d8d8a0438d
Vivek <vvivksingh@gmail.com>
2022-01-02 18:53:40 +0530

    AddressBook UC 2 Added

--- Day 10 Practice Problem/src/com/BridgeLabz/Day10/AddressBook/AddressBookMain.java
+++ Day 10 Practice Problem/src/com/BridgeLabz/Day10/AddressBook/AddressBookMain.java
@@ -1,24 +1,81 @@
 package com.BridgeLabz.Day10.AddressBook;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookMain {
-        // Create an arraylist to store contact details of all the persons
-        public static final ArrayList<ArrayList<String>>addressBook = new ArrayList<>();
+    // Create an arraylist to store contact details of all the persons
+    public static final ArrayList<ArrayList<String>> addressBook = new ArrayList<>();
 
-        // Use Case 1: Create contacts in address book
-        public void createContacts(ArrayList<String> contact) {
+    // Use Case 1: Create contacts in address book
+    public void createContacts(ArrayList<String> contact) {
 
-            // Add contact details of person to address book
-            AddressBookMain.addressBook.add(contact);
+        // Add contact details of person to address book
+        AddressBookMain.addressBook.add(contact);
 
-            // Print contact details of persons in address book
-            for (ArrayList<String> i : AddressBookMain.addressBook) {
-                for (String j : i) {
-                    System.out.println(j);
-                }
+        // Print contact details of persons in address book
+        for (ArrayList<String> i : AddressBookMain.addressBook) {
+            for (String j : i) {
+                System.out.println(j);
             }
         }
+    }
+
+    //Use case 2: Add  new contact to address book
+
+    public void addContact() {
+        ArrayList<String> contact = enterContactDetails();
+        createContacts(contact);
+    }
+
+    public ArrayList enterContactDetails() {
+        ArrayList<String> contact = new ArrayList<String>();
+
+        System.out.println("Enter the first name: ");
+        Scanner sc1 = new Scanner(System.in);
+        String first_name = sc1.next();
+        contact.add(first_name);
+
+        System.out.println("Enter the last name: ");
+        Scanner sc2 = new Scanner(System.in);
+        String last_name = sc2.next();
+        contact.add(last_name);
+
+        System.out.println("Enter the address: ");
+        Scanner sc3 = new Scanner(System.in);
+        String address = sc3.next();
+        contact.add(address);
+
+        System.out.println("Enter the city name: ");
+        Scanner sc4 = new Scanner(System.in);
+        String city = sc4.next();
+        contact.add(city);
+
+        System.out.println("Enter the state's name: ");
+        Scanner sc5 = new Scanner(System.in);
+        String state = sc5.next();
+        contact.add(state);
+
+        System.out.println("Enter the zip: ");
+        Scanner sc6 = new Scanner(System.in);
+        String zip = sc6.next();
+        contact.add(zip);
+
+        System.out.println("Enter the phone number: ");
+        Scanner sc7 = new Scanner(System.in);
+        String phone_num = sc7.next();
+        contact.add(phone_num);
+
+        System.out.println("Enter the email ID: ");
+        Scanner sc8 = new Scanner(System.in);
+        String email = sc8.next();
+        contact.add(email);
+
+        return contact;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program!");
+        AddressBookMain contactDetails = new AddressBookMain();
+        contactDetails.addContact();
     }
 }
