76aeab18f5ec47f1bca72bfbe7d5b15ff1421302
Vivek <vvivksingh@gmail.com>
2022-01-03 20:11:33 +0530

    Linked List UC3 added

---
+++ Day 14 Practice Problem/src/com/BridgeLabz/Day14/LinkedList/LlistUC3.java
@@ -0,0 +1,28 @@
+package com.BridgeLabz.Day14.LinkedList;
+import java.util.LinkedList;
+public class LlistUC3 extends LlistUC2{
+
+    //Use Case 3
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if (head == null){
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while(currNode.next != null){
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public static void main(String[] args) {
+        LlistUC3 l2 = new LlistUC3();
+        l2.addFirst(56);
+        l2.printList();
+        l2.addLast(30);
+        l2.printList();
+        l2.addLast(70);
+        l2.printList();
+    }
+}
