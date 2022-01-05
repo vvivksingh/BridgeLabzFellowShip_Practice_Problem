46c9833a3c14b74708db36e85a017f6241fcfcd2
Vivek <vvivksingh@gmail.com>
2022-01-05 15:22:05 +0530

    Linked list Use case 7 added

---
+++ Day 14 Practice Problem/src/com/BridgeLabz/Day14/LinkedList/LListSearchUC7.java
@@ -0,0 +1,55 @@
+package com.BridgeLabz.Day14.LinkedList;
+
+public class LListSearchUC7 {
+    public Node head;
+
+    public static class Node {
+        public int data;
+        public Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public static void main(String[] args) {
+            LListSearchUC7 lists = new LListSearchUC7();
+
+            lists.head = new Node(50);
+            Node secondNode = new Node(70);
+            Node thirdNode = new Node(30);
+            lists.head.next = secondNode;
+            secondNode.next = thirdNode;
+            System.out.println("before roving first node in data.");
+            lists.print();
+            lists.print();
+            search(lists, 30);
+        }
+
+        private static void search(LListSearchUC7 lists, int data) {
+            Node currentNode = lists.head, previousNode = null;
+
+            while (currentNode != null && currentNode.data != data) {
+
+                currentNode = currentNode.next;
+            }
+            System.out.println("node is found with data 30 : " + currentNode.data);
+        }
+
+    }
+
+    private void pop(Node node, LListSearchUC7 lists) {
+        Node courser = head;
+        System.out.println("after removing head node from link list.");
+
+    }
+
+    private void print() {
+        Node pointer = head;
+        while (pointer != null) {
+            System.out.print(pointer.data + "->");
+            pointer = pointer.next;
+        }
+        System.out.println("null");
+    }
+}
